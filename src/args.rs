a33105999b5da039700a4c6e020cbeb0b9010bf2
OLoKo64 <naldoepingo1@gmail.com>
2023-03-28 22:11:49 -0300

    feat: update deps

--- src/args.rs
+++ src/args.rs
@@ -1,57 +1,57 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-#[derive(clap::Subcommand)]
+#[derive(Subcommand)]
 pub enum ConversionOptions {
     /// Convert from one weight unit to metric
     Weight {
         /// Convert lbs to kilograms
-        #[clap(short, long)]
+        #[arg(short, long)]
         lbs: Option<f64>,
 
         /// Convert pounds to kilograms
-        #[clap(short, long)]
+        #[arg(short, long)]
         pounds: Option<f64>,
     },
 
     /// Convert from one time unit to metric
     Time {
         /// Converts GMT +0 time to Sao Paulo time
-        #[clap(short, long)]
+        #[arg(short, long)]
         gmt: Option<String>,
 
         /// Converts UTC +0 time to Sao Paulo time
-        #[clap(short, long)]
+        #[arg(short, long)]
         utc: Option<String>,
     },
 
     /// Convert from one length unit to metric
     Length {
         /// Convert miles to kilometers
-        #[clap(short, long)]
+        #[arg(short, long)]
         miles: Option<f64>,
 
         /// Convert feet to meters
-        #[clap(short, long)]
+        #[arg(short, long)]
         feet: Option<f64>,
 
         /// Convert inches to centimeters
-        #[clap(short, long)]
+        #[arg(short, long)]
         inches: Option<f64>,
 
         /// Convert yards to meters
-        #[clap(short, long)]
+        #[arg(short, long)]
         yards: Option<f64>,
     },
 }
 
-#[derive(clap::Parser)]
+#[derive(Parser)]
 pub struct Args {
     /// The conversion options
     #[command(subcommand)]
     pub conversion_options: ConversionOptions,
 
     /// The precision of the output, defaults to 2
-    #[clap(short, long, default_value = "2")]
+    #[arg(short, long, default_value = "2")]
     pub precision: Option<u8>,
 }
 
