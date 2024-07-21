67ecb4629414f8319e7fe69f671006aee94b3dae
OLoKo64 <naldoepingo1@gmail.com>
2024-07-21 13:02:23 -0300

    feat: update deps

--- src/conversions/mod.rs
+++ src/conversions/mod.rs
@@ -3,8 +3,7 @@ mod logic;
 mod time;
 
 pub use self::logic::{
-    feet_to_meters, gmt_to_sao_paulo, inches_to_centimeters, lbs_to_kg, miles_to_km,
-    yards_to_meters,
+    feet_to_meters, inches_to_centimeters, lbs_to_kg, miles_to_km, yards_to_meters,
 };
 pub use self::time::{print_time_conversion, ConversionType as TimeConversionType};
 pub use general::{print_general_conversion, ConversionType as GeneralConversionType};
